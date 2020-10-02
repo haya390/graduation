034ee25b79961691b075860031db5c6d0888c125
飯嶋貴臣 <72056788+iijima-tak@users.noreply.github.com>
2020-10-02 15:03:43 +0900

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,10 +2,18 @@
 #include<DxLib.h>
 
 int gametype = GAME_TITLE;
+int g_gametitleimg;
+
+BOOL KeyChecker(int key);
+BOOL g_akey_prev;
 
 int WINAPI WinMain(HINSTANCE h1, HINSTANCE hp, LPSTR lpc, int nC){
 	ChangeWindowMode(TRUE);
+	SetGraphMode(960, 840, 32);
 	if (DxLib_Init() == -1)return -1;
+
+	g_gametitleimg = LoadGraph("media\\smp1_title.png");
+
 
 	while (ProcessMessage() == 0){
 		switch (gametype)
@@ -26,6 +34,12 @@ int WINAPI WinMain(HINSTANCE h1, HINSTANCE hp, LPSTR lpc, int nC){
 	return 0;
 }
 void DrawTitle(){
+	DrawBox(0, 0, 800, 600, GetColor(255, 255, 255), TRUE);
+	DrawGraph(00, 0, g_gametitleimg, TRUE);
+
+	int key = GetJoypadInputState(DX_INPUT_KEY_PAD1);
+	if (KeyChecker(key) == TRUE) gametype = GAME_MAIN;
+
 
 }
 void DrawGameMain(){
@@ -37,6 +51,16 @@ void DrawGameOver(){
 void DrawGameClear(){
 
 }
-void KeyChecker(){
+int KeyChecker(int key){
+	if (key & PAD_INPUT_A){
+		if (g_akey_prev == FALSE){
+			g_akey_prev = TRUE;
+			return TRUE;
+		}
+	}
+	else{
+		g_akey_prev = FALSE;
+	}
+	return FALSE;
 
 }
--- main.h
+++ main.h
@@ -1,15 +1,16 @@
+
 #ifndef __MAIN_H__
 #define __MAIN_H__
 
 enum GameType
 {
-	GAME_TITLE = 0,GAME_MAIN = 1,GAME_OVER = 2,GAME_CLEAR = 3
+	GAME_TITLE = 0, GAME_MAIN = 1, GAME_OVER = 2, GAME_CLEAR = 3
 };
 
 void DrawTitle();
 void DrawGameMain();
 void DrawGameOver();
 void DrawGameClear();
-void keyChecker();
+int keyChecker(int);
 
 #endif
