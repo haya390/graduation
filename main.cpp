08532dcc3f297a9a28b54ccea4f99782f3df01c4
飯嶋貴臣 <72056788+iijima-tak@users.noreply.github.com>
2020-10-05 11:08:32 +0900

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@ int monitorsize_y = 840;
 int gamemainsize_x = 600;
 int gamemainsize_y = 800;
 
+int timerstart;
+int lasttime;
+
 int gametype = GAME_TITLE;
 BOOL g_akey_prev = FALSE;
 
@@ -16,6 +19,9 @@ int WINAPI WinMain(HINSTANCE h1, HINSTANCE hp, LPSTR lpc, int nC){
 	ChangeWindowMode(TRUE);
 	SetGraphMode(monitorsize_x, monitorsize_y, 32);
 	if (DxLib_Init() == -1)return -1;
+
+	
+	SetDrawScreen(DX_SCREEN_BACK);
 
 	while (ProcessMessage() == 0){
 		switch (gametype)
@@ -57,10 +63,14 @@ void DrawGameMain(){
 	}
 }
 void DrawGameOver(){
+	int largefont = CreateFontToHandle("メイリオ", 42, -1, DX_FONTTYPE_NORMAL);
 	SetGraphMode(monitorsize_x, monitorsize_y, 32);
 
-	DrawStringToHandle(480, 420, "左を押すとコンティニュー/右を押すとゲーム終了",GetColor(255,255,255),,32,FALSE);
-	
+	DrawStringToHandle(480, 420, "左を押すとコンティニュー/右を押すとゲーム終了", GetColor(255, 255, 255),largefont);
+
+	//int key = GetJoypadInputState(DX_INPUT_KEY_PAD1);
+	//SetGraphMode(gamemainsize_x, gamemainsize_y, 32);
+
 }
 void DrawGameClear(){
 
