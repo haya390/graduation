ae00e32adf0b65ebf3b086ec585614d5e0e911fe
金光希 <72056489+kanemitsu-nozomu@users.noreply.github.com>
2020-10-09 10:50:21 +0900

    Add files via upload

--- headerfile/cdflame.h
+++ headerfile/cdflame.h
@@ -4,14 +4,36 @@
 #include<DxLib.h>
 #include<stdio.h>
 
+enum attacktype{
+	red = 1, blue = 2, green = 3
+};
+enum type{
+	bullet = 1,beam = 2
+};
+
+struct BULLET{
+	float x; /*‰¡²*/
+	float y; /*c²*/
+	BOOL living; /*‘¶İØ–¾*/
+	int type; /*À’e/ƒr[ƒ€*/
+	float movement; /*ˆÚ“®—Ê*/
+};
+
 struct Charadata{
 	int LIFE;/*Å‘å”í’e‰ñ”*/
 	float movement;/*ˆÚ“®—Ê*/
 	int attacktype;/*‘®«*/
-	BOOL type;/*À’e/ƒr[ƒ€*/
-	float x;/*x²*/
-	float y;/*y²*/
-	BOOL living;/*’e/“G‚Ì‘¶İØ–¾*/
+	int type;/*À’e/ƒr[ƒ€*/
+	float x;/*‰¡²*/
+	float y;/*c²*/
+	BOOL living;/*‘¶İØ–¾*/
+	BULLET ENEMY_BULLET[5];
+	BULLET HERO_BULLET[20];
 };
+
+extern int hx,ex;
+extern int hy,ey;
+
+void syokika();
 
 #endif
