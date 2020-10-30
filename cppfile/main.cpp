4a93d5ffc83a17e38053e64348b1f6165dd0dded
飯嶋貴臣 <72056788+iijima-tak@users.noreply.github.com>
2020-10-30 14:31:03 +0900

    Add files via upload

--- cppfile/main.cpp
+++ cppfile/main.cpp
@@ -10,7 +10,10 @@ int gamemainsize_y = 800;
 int gametype = GAME_TITLE;
 BOOL g_akey_prev = FALSE;
 
+int s = GetNowCount();
 int KeyChecker(int);
+
+int g_timestart = 0;
 
 int WINAPI WinMain(HINSTANCE h1, HINSTANCE hp, LPSTR lpc, int nC){
 	SetWindowSizeChangeEnableFlag(TRUE);
@@ -34,7 +37,7 @@ int WINAPI WinMain(HINSTANCE h1, HINSTANCE hp, LPSTR lpc, int nC){
 			DrawGameMain();
 			break;
 		case GAME_OVER:
- 			DrawGameOver();
+			DrawGameOver();
 			break;
 		case GAME_CLEAR:
 			DrawGameClear();
@@ -51,7 +54,6 @@ void DrawTitle(){
 
 	int key = GetJoypadInputState(DX_INPUT_KEY_PAD1);
 	if (KeyChecker(key) == TRUE){
-		syokika();
 		gametype = GAME_MAIN;
 		InitStage(1);
 		SetGraphMode(gamemainsize_x, gamemainsize_y, 32);
@@ -64,30 +66,47 @@ void DrawGameMain(){
 		gametype = GAME_OVER;
 		SetGraphMode(monitorsize_x, monitorsize_y, 32);
 		IMGhandle();
+		g_timestart = GetNowCount();
 	}
 	gamemain();
 }
 void DrawGameOver(){
+	int g_nFrame = 0;//
+	LONGLONG Time;//
+	
+	int Cr = GetColor(255, 255, 255);
+	int Timecount = 0;
 	int largefont = CreateFontToHandle("メイリオ", 42, -1, DX_FONTTYPE_NORMAL);
+	int Timefont = CreateFontToHandle("メイリオ", 50, -1, DX_FONTTYPE_NORMAL);
 
 	DrawStringToHandle(200, 200, "左を押すとコンティニュー", GetColor(255, 255, 255), largefont, 32, FALSE);
 	DrawStringToHandle(200, 250, "右を押すとゲーム終了", GetColor(255, 255, 255), largefont, 32, FALSE);
+	Time = GetNowCount();//
 
-	if (CheckHitKey(KEY_INPUT_LEFT) == TRUE){
+		// ゲーム時間をカウント
+		g_nFrame++;
+		// 終了処理
+
+		if (CheckHitKey(KEY_INPUT_LEFT) == TRUE){
+			gametype = GAME_MAIN;
+			SetGraphMode(gamemainsize_x, gamemainsize_y, 32);
+			IMGhandle();
+			
+			/*時間処理*/
+		}
+		else if (CheckHitKey(KEY_INPUT_RIGHT) == TRUE){
+			exit(0);
+		}
 		
-		gametype = GAME_MAIN;
-		SetGraphMode(gamemainsize_x, gamemainsize_y, 32);
-		IMGhandle();
-		/*時間処理*/
-
-	}
-	else if (CheckHitKey(KEY_INPUT_RIGHT) == TRUE){
-		exit(0);
-	}
-
-}
-void DrawGameClear(){
+		if (Time - g_timestart >= 5000){
+			gametype = GAME_TITLE;
+			
+		}
 	
+}
+
+void DrawGameClear(){
+
 }
 int KeyChecker(int key){
 	if (key & PAD_INPUT_A){
