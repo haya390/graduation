9b065a486b776e06ac6cad26a11b7bceed015909
金光希 <72056489+kanemitsu-nozomu@users.noreply.github.com>
2020-12-02 12:11:32 +0900

    Create loading.h

---
+++ space_shooting/loading.h
@@ -0,0 +1,22 @@
+#ifndef __LOADING_H__
+#define __LOADING_H__
+
+#include<DxLib.h>
+
+enum youso
+{
+	enemy1 = 1,enemy2,enemy3,player,boss
+};
+
+extern int G_IMGhandle[10][13];
+extern int field;
+extern int gif[4][12];
+extern int BOSS;
+extern int TITLE;
+extern int GAMEOVER;
+extern int BULLET[4];
+
+BOOL IMGhandle();
+BOOL InitBulletImage(int);
+
+#endif
