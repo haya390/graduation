711dc4b44640c4517e129ae714ff4ea01cd53138
haya390 <75233234+haya390@users.noreply.github.com>
2021-01-12 13:50:54 +0900

    Update cdprocess.cpp

--- space_shooting/cdprocess.cpp
+++ space_shooting/cdprocess.cpp
@@ -7,14 +7,6 @@ float scrolly = 0;
 void move(){
 	int key = GetJoypadInputState(DX_INPUT_KEY_PAD1);
 
-	int aa = timer;
-	float hy = HERO.y - 1700;
-	aa += 1;
-	if (timer < aa)
-	{
-		//scrolly += 1;
-	}
-
 	if (key & PAD_INPUT_UP && CheckEnd(1) == TRUE){
 		HERO.y -= HERO.movement;
 		CheckScroll(1);
@@ -39,10 +31,10 @@ BOOL CheckEnd(int i){
 	switch (i)
 	{
 	case 1:
-		if (HERO.y - HERO.movement < 0) return FALSE;
+		if (HERO.y - HERO.movement <= 0) return FALSE;
 		break;
 	case 2:
-		if (SHP * IMGSIZE + ((50 - SHP) * IMGSIZE) < HERO.y + IMGSIZE + HERO.movement)return FALSE;
+		if (SHP * IMGSIZE + ((50 - SHP) * IMGSIZE) <= HERO.y + IMGSIZE + HERO.movement)return FALSE;
 		break;
 	case 3:
 		if (HERO.x - HERO.movement < 0)return FALSE;
@@ -62,8 +54,8 @@ void CheckScroll(int flag){
 	int field_head = 0;
 	
 	if (HERO.y >= 0 && HERO.y <= 800){
-		if (flag == 2){
-			scrolly -= HERO.movement;
+		if (HERO.y + IMGSIZE + HERO.movement - HERO.SC_Hosei > 800){
+			HERO.y -= HERO.movement;
 		}
 	}
 	if (HERO.y > 800 && HERO.y <= 1950){
@@ -76,40 +68,58 @@ void CheckScroll(int flag){
 	}
 	if (HERO.y > 1950 && HERO.y <= 2750){
 		if (flag == 1){
-			scrolly -= HERO.movement;
+			scrolly += HERO.movement;
+		}
+		if (flag == 2 && scrolly == 0){
+			HERO.SC_Hosei += HERO.movement;
 		}
 	}
-	
-	/*画面外に出ないように補正をかける*/
 
+	/*画面外へ出ないように補正*/
+	if ((HERO.y + IMGSIZE) > MAX_MONITOR_SIZE - scrolly){
+		HERO.y -= HERO.movement;
+	}
+	if ((SHP * IMGSIZE - HERO.SC_Hosei ) > (HERO.y - HERO.movement)){
+		HERO.y += HERO.movement;
+	}
+	
 }
 void DrawBullet(int flag){
 	int largefont = CreateFontToHandle("メイリオ", 42, -1, DX_FONTTYPE_NORMAL);
 
 	if (flag == E){
 
-	}else if (flag == P){
-		for (int i = 0; HERO.BULLET[i].living == TRUE; i++){
-			switch (HERO.BULLET[i].type)
-			{
-			case red:
-				DrawGraph(HERO.BULLET[i].x, HERO.BULLET[i].y - 1950, HERO.BULLET[i].BULLET_IMAGE, TRUE);
-				break;
-			case blue:
-				DrawGraph(HERO.BULLET[i].x, HERO.BULLET[i].y - 1950, HERO.BULLET[i].BULLET_IMAGE, TRUE);
-				break;
-			case green:
-				DrawGraph(HERO.BULLET[i].x, HERO.BULLET[i].y - 1950, HERO.BULLET[i].BULLET_IMAGE, TRUE);
-				break;
-			case beam:
-				DrawExtendGraph(HERO.BULLET[i].x,HERO.BULLET[i].y - 1950,HERO.BULLET[i].x + IMGSIZE,HERO.BULLET[i].y - 1950 + IMGSIZE,HERO.BULLET[i].BULLET_IMAGE,TRUE);
-				break;
-			}
-			if (HERO.BULLET[i].y + IMGSIZE <= (HERO.y + 110 - gamemainsize_y)){
-				HERO.BULLET[i].living = FALSE;
-			}
-			if(HERO.BULLET[i].living == TRUE){
-				HERO.BULLET[i].y -= HERO.BULLET[i].movement;
+	}
+	else if (flag == P){
+		for (int i = 0; i < 20; i++){
+			if (HERO.BULLET[i].living == TRUE){
+				switch (HERO.BULLET[i].type)
+				{
+				case red:
+					DrawGraph(HERO.BULLET[i].x, HERO.BULLET[i].y - 1950 + scrolly, HERO.BULLET[i].BULLET_IMAGE, TRUE);
+					break;
+				case blue:
+					DrawGraph(HERO.BULLET[i].x, HERO.BULLET[i].y - 1950 + scrolly, HERO.BULLET[i].BULLET_IMAGE, TRUE);
+					break;
+				case green:
+					DrawGraph(HERO.BULLET[i].x, HERO.BULLET[i].y - 1950 + scrolly, HERO.BULLET[i].BULLET_IMAGE, TRUE);
+					break;
+				case beam:
+					DrawExtendGraph(HERO.BULLET[i].x, HERO.BULLET[i].y - 1950 + scrolly, HERO.BULLET[i].x + IMGSIZE, HERO.BULLET[i].y - 1950 + scrolly + IMGSIZE, HERO.BULLET[i].BULLET_IMAGE, TRUE);
+					break;
+				}
+				if (HERO.BULLET[i].living == TRUE && HERO.BULLET[i].SBP - 800 > HERO.BULLET[i].y){
+					HERO.BULLET[i].BULLET_IMAGE = NULL;
+					HERO.BULLET[i].living = FALSE;
+					HERO.BULLET[i].movement = 0;
+					HERO.BULLET[i].SBP = 0;
+					HERO.BULLET[i].time = 0;
+					HERO.BULLET[i].x = 0;
+					HERO.BULLET[i].y = 0;
+				}
+				if (HERO.BULLET[i].living == TRUE){
+					HERO.BULLET[i].y -= HERO.BULLET[i].movement;
+				}
 			}
 		}
 	}
